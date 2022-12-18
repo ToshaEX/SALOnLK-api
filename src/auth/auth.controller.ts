1e7fad9e9d11032b64dfb8ccdf87fbd4e1dd85ee
ToshaEX <tharinduoshan@gmail.com>
2022-12-18 20:21:11 +0530

    swagger setup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { AuthService } from './auth.service';
+import { SignUpDto } from './dto/sign-up.dto';
 
+@ApiBearerAuth()
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('/signup')
+  @ApiOperation({ summary: 'Sign up' })
+  @ApiResponse({ status: 403, description: 'Forbidden.' })
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.signUp(signUpDto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { SignUpDto } from './dto/sign-up.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async signUp(signUpDto: SignUpDto): Promise<string> {
+    console.log(signUpDto);
+    return 'success';
+  }
+
+}
