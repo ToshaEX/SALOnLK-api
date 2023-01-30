e8a71527198ac6b1d32d2f2545a382bae96a18b2
ToshaEX <tharinduoshan@gmail.com>
2023-01-30 20:16:04 +0530

    done

--- src/appointment/dto/create-appointment.dto.ts
+++ src/appointment/dto/create-appointment.dto.ts
@@ -1,25 +1,25 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsArray, IsDateString, IsNotEmpty } from 'class-validator';
 export class CreateAppointmentDto {
-  @ApiProperty({ name: 'Appointment date', type: Date })
+  @ApiProperty({  type: Date })
   @IsNotEmpty()
   @IsDateString()
   appointment_date: Date;
 
-  @ApiProperty({ name: 'Slots', type: Number, isArray: true })
+  @ApiProperty({  type: Number, isArray: true })
   @IsNotEmpty()
   @IsArray()
   slots: number[];
 
-  @ApiProperty({ name: 'Beautician ID', type: String })
+  @ApiProperty({  type: String })
   @IsNotEmpty()
   beautician: string;
 
-  @ApiProperty({ name: 'User ID', type: String })
+  @ApiProperty({ type: String })
   @IsNotEmpty()
   user: string;
 
-  @ApiProperty({ name: 'Service ID Array', type: String, isArray: true })
+  @ApiProperty({  type: String, isArray: true })
   @IsNotEmpty()
   services: string[];
 }
--- src/appointment/dto/update-status.dto.ts
+++ src/appointment/dto/update-status.dto.ts
@@ -3,7 +3,7 @@ import { Status } from './../enum/status.enum';
 import { IsEnum, IsNotEmpty } from 'class-validator';
 
 export class UpdateStatusDto {
-  @ApiProperty({ name: 'Appointment Status',enum:Status })
+  @ApiProperty({ enum:Status })
   @IsNotEmpty()
   @IsEnum(Status)
   is_approved: Status;
